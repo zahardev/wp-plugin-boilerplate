38b109b49c01cfedb6a0d704a08890e61cd03612
Sergey Zaharchenko <zaharchenko.dev@gmail.com>
2018-09-12 18:58:11 +0300

    Create LICENSE, changed plugin description

--- wp-plugin-boilerplate.php
+++ wp-plugin-boilerplate.php
@@ -11,13 +11,13 @@ it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
 any later version.
 
-Stripe Payments Custom Fields is distributed in the hope that it will be useful,
+{WP_Plugin_Boilerplate} is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Stripe Payments Custom Fields. If not, see {License URI}.
+along with Stripe Payments Custom Fields. If not, see https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html.
 
 */
 
